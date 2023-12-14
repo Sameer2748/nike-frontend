12e61c0dc6f051ffa730864b752a8b07638790af
Manish Sameer <sameer@Manishs-MacBook-Air.local>
2023-12-14 21:32:25 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import Subscibe from "./Subscibe";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+export {Hero, Services, PopularProducts, Subscibe, Footer, SpecialOffer, SuperQuality, CustomerReviews }
